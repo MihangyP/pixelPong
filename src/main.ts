59330652da33bf30b72917ffd53b922fdad606d1
Pierrot Mihangy <pmihangy@c1r4s8.42antananarivo.mg>
2026-02-23 11:40:20 +0300

    Particles effects

--- src/main.ts
+++ src/main.ts
@@ -59,30 +59,54 @@ let playerMoveLeft = false;
 const playerVelocity = 300;
 let paused = false;
 
-const PARTICLE_QUANTITY = 10;
-const PARTICLE_SIZE = 5;
-const PARTICLE_COLOR = "red";
-const PARTICLE_LIFE = 2; // in second
-const PARTICLE_MAX_SPEED = 12;
+const PARTICLE_QUANTITY = 25;
+const PARTICLE_SIZE = 3;
+const PARTICLE_LIFE = 1.8; // in second
+const PARTICLE_MAX_SPEED = 40;
+const FADE_FRACTION = 0.4;
+
+interface RgbaColor {
+	r: number,
+	g: number,
+	b: number,
+	a: number,
+}
 
 interface Rect {
 	pos: Vector2,
 	width: number,
 	height: number,
-	color: string,
+	color: RgbaColor,
 };
 
 interface Particle {
 	rect: Rect,
 	vx: number,
 	vy: number,
-	lifetime: number
+	lifetime: number,
+	birthtime: number,
+}
+
+function lerp(start: number, end: number, t: number): number {
+	return (start + (end - start) * t);
+}
+
+function hexToRgba(hex: string): RgbaColor {
+	hex = hex.replace('#', '');
+	const hexInt = parseInt(hex, 16);
+	return {
+		r: (hexInt >> 16) & 255,
+		g: (hexInt >> 8) & 255,
+		b: hexInt & 255,
+		a: 1,
+	};
 }
 
 let particlesArray: Particle[][] = [];
 
-function generateParticles(pos: Vector2) {
+function generateParticles(pos: Vector2, color: RgbaColor) {
 	const particles: Particle[] = [];
+	const now = performance.now();
 
 	for (let i = 0; i < PARTICLE_QUANTITY; ++i) {
 		const angle = Math.random() * Math.PI * 2;
@@ -91,11 +115,17 @@ function generateParticles(pos: Vector2) {
 				pos: {x: pos.x, y: pos.y},
 				width: PARTICLE_SIZE,
 				height: PARTICLE_SIZE,
-				color: PARTICLE_COLOR
+				color: {
+					r: color.r,
+					g: color.g,
+					b: color.b,
+					a: color.a
+				}
 			},
 			vx: (Math.random() * PARTICLE_MAX_SPEED) * Math.cos(angle),
 			vy: (Math.random() * PARTICLE_MAX_SPEED) * Math.sin(angle),
-			lifetime: PARTICLE_LIFE
+			lifetime: PARTICLE_LIFE,
+			birthtime: now,
 		};
 		particles.push(particle);
 	}
@@ -106,20 +136,38 @@ function drawParticles(ctx: CanvasRenderingContext2D): void {
 	for (let i = 0; i < particlesArray.length; ++i) {
 		for (let j = 0; j < PARTICLE_QUANTITY; ++j) {
 			const particle = particlesArray[i]?.[j];
-			if (particle)
-				drawRectangle(ctx, particle.rect.pos, particle.rect.width, particle.rect.height, particle.rect.color);
+			if (particle) {
+				const {r, g, b, a} = particle.rect.color;
+				const color = `rgba(${r}, ${g}, ${b}, ${a})`;
+				drawRectangle(ctx, particle.rect.pos, particle.rect.width, particle.rect.height, color);
+			}
 		}
 	}
 }
 
+// TODO: FADEOUT
 function updateParticles(dt: number) {
 	for (let i = 0; i < particlesArray.length; ++i) {
-		for (let j = 0; j < PARTICLE_QUANTITY; ++j) {
-			const particle = particlesArray[i]?.[j];
-			if (particle) {
-				particle.rect.pos.x += particle.vx * dt;
-				particle.rect.pos.y += particle.vy * dt;
+		const group = particlesArray[i];
+		if (!group) break;
+		for (let j = group.length - 1; j >= 0; --j) {
+			const p = group[j];
+			if (!p) continue;
+			p.lifetime -= dt;
+			if (p.lifetime <= 0) {
+				group.splice(j, 1);
+			} else {
+				p.rect.pos.x += p.vx * dt;
+				p.rect.pos.y += p.vy * dt;
+
+				if (p.lifetime < PARTICLE_LIFE * FADE_FRACTION) {
+					const t = p.lifetime / (PARTICLE_LIFE * FADE_FRACTION);
+					p.rect.color.a = lerp(1, 0, 1 - t);
+				}
 			}
+		}
+		if (group.length === 0) {
+			particlesArray.splice(i, 1);
 		}
 	}
 }
@@ -219,17 +267,36 @@ function drawAuth(ctx: CanvasRenderingContext2D) {
 
 function playGame(ctx: CanvasRenderingContext2D, dt: number) {
 	if (!paused) {
-		if (ballPos.x + ballRadius >= windowWidth || ballPos.x - ballRadius <= 0) {
+		if (ballPos.x + ballRadius >= windowWidth) {
 			ballVelocity.x *= -1;
-			generateParticles({x: (windowWidth - PARTICLE_SIZE) / 2, y: (windowHeight - PARTICLE_SIZE) / 2});
+			const color = hexToRgba(myWhite);
+			generateParticles({x: ballPos.x + ballRadius, y: ballPos.y}, color);
 		}
-		if (ballPos.y + ballRadius >= windowHeight || ballPos.y - ballRadius <= 0) {
+		if (ballPos.x - ballRadius <= 0) {
+			ballVelocity.x *= -1;
+			const color = hexToRgba(myWhite);
+			generateParticles({x: ballPos.x - ballRadius, y: ballPos.y}, color);
+		}
+		if (ballPos.y + ballRadius >= windowHeight) {
 			ballVelocity.y *= -1;
+			const color = hexToRgba(myWhite);
+			generateParticles({x: ballPos.x, y: ballPos.y + ballRadius}, color);
+		}
+		if (ballPos.y - ballRadius <= 0) {
+			ballVelocity.y *= -1;
+			const color = hexToRgba(myWhite);
+			generateParticles({x: ballPos.x, y: ballPos.y - ballRadius}, color);
+		}
+		if (checkCollisionRecCircle(playerPos, paddleWidth, paddleHeight, ballPos, ballRadius)) {
+			ballVelocity.x *= -1;
+			const color = hexToRgba(playerPaddleColor);
+			generateParticles({x: ballPos.x, y: ballPos.y}, color);
 		}
 
-		if (checkCollisionRecCircle(playerPos, paddleWidth, paddleHeight, ballPos, ballRadius) ||
-			checkCollisionRecCircle(botPos, paddleWidth, paddleHeight, ballPos, ballRadius)) {
+		if (checkCollisionRecCircle(botPos, paddleWidth, paddleHeight, ballPos, ballRadius)) {
 			ballVelocity.x *= -1;
+			const color = hexToRgba(botPaddleColor);
+			generateParticles({x: ballPos.x, y: ballPos.y}, color);
 		}
 
 		ballPos.x += ballVelocity.x * dt;
